a889ee1edc3c6678ce9e2cf1cb067f0c625380be
Melvin Byrd <xb12mel@gmail.com>
2021-02-24 23:42:56 -0500

    Adding app.py
    It's the second portion of the scraping assignment

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, redirect, jsonify
+from flask_pymongo import PyMongo
+import  scrape_assignment import scrape_assignment
+
+app=Flask(__name__)
+
+app.config['MONGO_URI'] = 'mongodb://localhost:2017/mars_mission'
+mongo = PyMongo(app)
+@app.route('/')
+def index():
+    posts = mongo.db.posts.find()
+    return render_template("index.html", mars= )
+
+@app.route('/scrape_assignement')
+def scrape_assignment():
+    mars_mission = mongo.db.mars_mission
+    data = scrape_assignment.scrape()
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
