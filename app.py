ee203e8a0a2207fdedd65c135b74bb69e3ac93b2
Melvin Byrd <xb12mel@gmail.com>
2021-02-25 23:25:12 -0500

    Adjusting File Structure
    Putting files in proper tree structure for the Flask App.

--- app.py
+++ app.py
@@ -1,21 +1,32 @@
-from flask import Flask, render_template, redirect, jsonify
-from flask_pymongo import PyMongo
-import  scrape_assignment import scrape_assignment
+from flask import Flask, render_template, redirect, url_for, request
 
+# from flask_pymongo import PyMongo
+# import scrape_mars import scrape_mars
+
+#create instance of Flask app
 app=Flask(__name__)
 
-app.config['MONGO_URI'] = 'mongodb://localhost:2017/mars_mission'
-mongo = PyMongo(app)
-@app.route('/')
+# hemisphere_image_urls = [
+#     {"title":"Cerberus", "img_url": cerberus},
+#     {"title":"Schiaparelli", "img_url":schiaparelli},
+#     {"title":"Surtis", "img_url":syrtis},
+#     {"title":"Valles", "img_url":valles}
+# ]
+
+#create rout that renders index.html template
+@app.route("/")
+
 def index():
-    posts = mongo.db.posts.find()
-    return render_template("index.html", mars= )
-
-@app.route('/scrape')
-def scrape_mars():
-    mars_mission = mongo.db.mars_mission
-    data = scrape_mars.scrape()
+    return render_template("index.html", hemisphere_image_urls=hemisphere_image_urls)
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+
+
+
+    if __name__ == "__main__":
+        app.run(debug=True)
+
+
+
+
+    
